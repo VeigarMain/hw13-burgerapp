81d8c60b667d3b5c918c3b1ce7f7dccbe440cc67
Jake Battaglia <jbaked12@gmail.com>
2020-09-15 15:36:44 -0400

    tried to fix burger_name error, no luck. added a lot more styling/css

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,42 @@
 const connection = require("./connection.js");
 
 
+function printQuestionMarks(num) {
+  var arr = [];
+  //returns a string of "?, ?, ?" with a 'num' number of question marks
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+//Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  //loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    //check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      //if string with spaces, add quotations (Michael Jordan => 'Michael Jordan')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      //{sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
+
+  //return single comma-separated string
+  return arr.toString();
+}
+
+
+
+
+
+
 const orm = {
     selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + ";";
--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id int AUTO_INCREMENT NOT NULL,
+    id int  NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(45) NOT NULL,
     devoured BOOLEAN DEFAULT false, 
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
